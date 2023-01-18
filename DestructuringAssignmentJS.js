9d9eb90ddb1d117ddd410c586789f10aa0f598e3
Elyzavetka <lizamybella@gmail.com>
2023-01-18 21:57:43 +0000

    Update DestructuringAssignmentJS.js

--- DestructuringAssignmentJS.js
+++ DestructuringAssignmentJS.js
@@ -19,3 +19,21 @@ const [fruitOne, fruitTwo] = fruits;
 
 console.log(fruitOne);
 console.log(fruitTwo);
+
+//Function
+
+const userProfile1 = {
+  name1: `Bogdan`,
+  commentsQty: 23,
+  hasSignedAgreement: false,
+};
+
+const userInfo = ({ name1, commentsQty }) => {
+  if (!commentsQty) {
+    return `User ${name1}  has no comments`;
+  }
+
+  return `User ${name1} has ${commentsQty} comments`;
+};
+
+userInfo(userProfile1);
